3f5c942dc3c4806594fddb9853cf30c4acd01a0b
PointMeAtTheDawn <croselius@gmail.com>
2019-05-28 22:54:59 -0700

    Removing unused code

--- convert.py
+++ convert.py
@@ -1,15 +1,15 @@
 from PIL import Image
 from pdf2image import convert_from_path
 
-def crop(image_path, coords, saved_location):
-    """
-    @param image_path: The path to the image to edit
-    @param coords: A tuple of x/y coordinates (x1, y1, x2, y2)
-    @param saved_location: Path to save the cropped image
-    """
-    image_obj = Image.open(image_path)
-    cropped_image = image_obj.crop(coords)
-    cropped_image.save(saved_location)
+def parse_images(fronts, backs, page):
+    fronts.append(page.crop((94,151,592,852)))
+    fronts.append(page.crop((597,151,1095,852)))
+    fronts.append(page.crop((1099,151,1598,852)))
+    fronts.append(page.crop((1602,151,2101,852)))
+    backs.append(page.crop((94,855,592,1553)))
+    backs.append(page.crop((597,855,1095,1553)))
+    backs.append(page.crop((1099,855,1598,1553)))
+    backs.append(page.crop((1602,855,2101,1553)))
  
 if __name__ == '__main__':
     infile = 'cardbundle.pdf'
@@ -17,14 +17,7 @@ if __name__ == '__main__':
     cardFronts = []
     cardBacks = []
     for page in pages:
-        cardFronts.append(page.crop((94,151,592,852)))
-        cardFronts.append(page.crop((597,151,1095,852)))
-        cardFronts.append(page.crop((1099,151,1598,852)))
-        cardFronts.append(page.crop((1602,151,2101,852)))
-        cardBacks.append(page.crop((94,855,592,1553)))
-        cardBacks.append(page.crop((597,855,1095,1553)))
-        cardBacks.append(page.crop((1099,855,1598,1553)))
-        cardBacks.append(page.crop((1602,855,2101,1553)))
+        parse_images(cardFronts, cardBacks, page)
     
     for i in range(len(cardFronts)//40+1):
         fronts = Image.new('RGB',(3984,3505))
