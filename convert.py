807863a4ce32bb583ae83abdc872793b19f03b55
PointMeAtTheDawn <croselius@gmail.com>
2019-05-30 10:22:17 -0700

    Resizes images to fit the power of 2 preferred sizing for TTS. Imgur still downsamples like a mofo. Gonna swap to GDrive next.

--- convert.py
+++ convert.py
@@ -2,6 +2,7 @@ import requests
 import argparse
 import os
 import threading
+import PIL.ImageOps
 from PIL import Image
 from pdf2image import convert_from_path
 from base64 import b64encode
@@ -33,19 +34,23 @@ def imgur_upload(args, links, image, name):
     )
     print(r.json())
     links[name] = r.json()["data"]["link"]
-    os.remove(name)
+    #os.remove(name)
 
 def package_page(args, links, cardFronts, cardBacks):
-    fronts = Image.new('RGB',(2988,3505))
-    backs = Image.new('RGB',(2988,3505))
-    for j in range(30):
-        if len(cardFronts) <= i*30+j:
+    width = 410
+    height = 585
+    fronts = Image.new('RGB',(4096,4096))
+    backs = Image.new('RGB',(4096,4096))
+    for j in range(70):
+        if len(cardFronts) <= i*70+j:
             continue
-        fronts.paste(cardFronts[i*30+j], (j%6*498,(j//6)*701))
-        backs.paste(cardBacks[i*30+j].rotate(180), (j%6*498,(j//6)*701))
-    t1 = threading.Thread(target=imgur_upload, args=(args,links,fronts,f'f-{i}.png'))
+        front = cardFronts[i*70+j].resize((width, height), Image.BICUBIC)
+        back = cardBacks[i*70+j].resize((width, height), Image.BICUBIC).rotate(180)
+        fronts.paste(front, (j%10*width,(j//10)*height))
+        backs.paste(back, (j%10*width,(j//10)*height))
+    t1 = threading.Thread(target=imgur_upload, args=(args,links,fronts,f'f-{i}.jpg'))
     t1.start()
-    t2 = threading.Thread(target=imgur_upload, args=(args,links,backs,f'b-{i}.png'))
+    t2 = threading.Thread(target=imgur_upload, args=(args,links,backs,f'b-{i}.jpg'))
     t2.start()
     t1.join()
     t2.join()
@@ -53,8 +58,8 @@ def package_page(args, links, cardFronts, cardBacks):
 def write_deck(deckJSON, args, links, num):
     name = args.faction + str(num)
     deckJSON = deckJSON.replace("DeckName", name)
-    deckJSON = deckJSON.replace("FrontImageURL", links[f'f-{i}.png'])
-    deckJSON = deckJSON.replace("BackImageUrl", links[f'b-{i}.png'])
+    deckJSON = deckJSON.replace("FrontImageURL", links[f'f-{i}.jpg'])
+    deckJSON = deckJSON.replace("BackImageUrl", links[f'b-{i}.jpg'])
     deckJSON = deckJSON.replace("ReplaceGUID", f'{name}C')
     deckJSON = deckJSON.replace("ReplaceGUID2", f'{name}D')
     with open(args.savedObjectsFolder+name+".json", 'w') as deck:
@@ -77,20 +82,28 @@ if __name__ == '__main__':
     for page in pages:
         parse_images(cardFronts, cardBacks, page)
     
+    # But we don't want the blank white cards.
+    filteredFronts = []
+    filteredBacks = []
+    for i in range(len(cardFronts)):
+        # I'd kinda rather do a .filter on these but I'm concerned that there'd be some random stray pixel that would put them outta sync.
+        if PIL.ImageOps.invert(cardFronts[i]).getbbox():
+            filteredFronts.append(cardFronts[i])
+            filteredBacks.append(cardBacks[i])
+    
     # Collate the cards into the image format Tabletop Simulator requires.
     links = {}
-    deckCount = len(cardFronts)//30+1
-
+    deckCount = len(cardFronts)//70+1
     for i in range(deckCount):
-        package_page(args, links, cardFronts, cardBacks)
+        package_page(args, links, filteredFronts, filteredBacks)
     
     # Wait for all of the imgur uploads to finish - nvm, this causes a MemoryError I'm too lazy to track down :p.
     # for thread in threads:
     #     thread.join()
 
+    # And let's shove em all in your Saved Objects folder :)
     deckJSON = ''
     with open('decktemplate.json', 'r') as deckTemplate:
-        deckJSON = deckTemplate.read()
-    
+        deckJSON = deckTemplate.read()    
     for i in range(deckCount):
         write_deck(deckJSON, args, links, i)
