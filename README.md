21ca9798bd557b8b140c4e9cf1c3724f296a7233
PointMeAtTheDawn <croselius@gmail.com>
2019-05-30 19:36:30 -0700

    Now works with google drive instead of imgur! If only I'd listened that that isn't a great host for TTS >:(. Next up, hosting in steam cloud.

---
+++ README.md
@@ -0,0 +1,8 @@
+Prerequisites:
+* [Python](https://www.python.org/downloads/)
+* Modules: `pip install -r requirements.txt`
+
+Usage:
+`python convert.py -name DECKNAME -savedObjectsFolder "Saved\Objects\Folder\\"`
+* Deckname could be CRYX or something if you want to do each faction separately.
+* If you don't provide your saved objects folder, it will save your deck JSONs to this directory and you will need to copy them over yourself. You'll need to enclose the directory name with quotes, and end with \\\ because I'm lazy.
--- convert.py
+++ convert.py
@@ -1,109 +1,165 @@
-import requests
-import argparse
-import os
+"""This converts a cardbundle.pdf (downloaded from Privateer Press) into
+   Tabletop Simulator deck Saved Objects."""
+
+import pickle
+import os.path
 import threading
+import argparse
 import PIL.ImageOps
 from PIL import Image
 from pdf2image import convert_from_path
-from base64 import b64encode
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-def parse_images(fronts, backs, page):
-    fronts.append(page.crop((94,151,592,852)))
-    fronts.append(page.crop((597,151,1095,852)))
-    fronts.append(page.crop((1099,151,1598,852)))
-    fronts.append(page.crop((1602,151,2101,852)))
-    backs.append(page.crop((94,855,592,1553)))
-    backs.append(page.crop((597,855,1095,1553)))
-    backs.append(page.crop((1099,855,1598,1553)))
-    backs.append(page.crop((1602,855,2101,1553)))
+SCOPES = ["https://www.googleapis.com/auth/drive.file"]
 
-def imgur_upload(args, links, image, name):
+
+def parse_images(fronts, backs, raw_page):
+    """Chop a page from the PP PDF into its constituent card images."""
+    fronts.append(raw_page.crop((94, 151, 592, 852)))
+    fronts.append(raw_page.crop((597, 151, 1095, 852)))
+    fronts.append(raw_page.crop((1099, 151, 1598, 852)))
+    fronts.append(raw_page.crop((1602, 151, 2101, 852)))
+    backs.append(raw_page.crop((94, 855, 592, 1553)))
+    backs.append(raw_page.crop((597, 855, 1095, 1553)))
+    backs.append(raw_page.crop((1099, 855, 1598, 1553)))
+    backs.append(raw_page.crop((1602, 855, 2101, 1553)))
+
+
+def drive_upload(image, name, links):
+    """Upload a compiled TTS-compatible deck template image into Google Drive."""
     image.save(name)
-        
-    headers = {"Authorization": f'Client-ID {args.imgurClientID}'}
-    r = requests.post(
-        'https://api.imgur.com/3/upload.json',
-        headers = headers,
-        data = {
-            'key': args.imgurAPIKey,
-            'image': b64encode(open(name, 'rb').read()),
-            'type': 'base64',
-            'name': name,
-            'title': name
-        }
-    )
-    print(r.json())
-    links[name] = r.json()["data"]["link"]
-    #os.remove(name)
 
-def package_page(args, links, cardFronts, cardBacks):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    service = build("drive", "v3", credentials=creds)
+    file_metadata = {"name": name}
+    media = MediaFileUpload(name, mimetype="image/jpeg")
+    file = (
+        service.files()
+        .create(body=file_metadata, media_body=media, fields="id")
+        .execute()
+    )
+
+    media = MediaFileUpload(name, mimetype="image/jpeg")
+    file = (
+        service.files()
+        .create(body=file_metadata, media_body=media, fields="id")
+        .execute()
+    )
+
+    links[name] = "https://drive.google.com/uc?export=download&id=" + file.get("id")
+    os.remove(name)
+
+
+def package_pages(fronts, backs, page_count, links):
+    """Stitch together card images into a TTS-compatible deck template image"""
     width = 410
     height = 585
-    fronts = Image.new('RGB',(4096,4096))
-    backs = Image.new('RGB',(4096,4096))
-    for j in range(70):
-        if len(cardFronts) <= i*70+j:
-            continue
-        front = cardFronts[i*70+j].resize((width, height), Image.BICUBIC)
-        back = cardBacks[i*70+j].resize((width, height), Image.BICUBIC).rotate(180)
-        fronts.paste(front, (j%10*width,(j//10)*height))
-        backs.paste(back, (j%10*width,(j//10)*height))
-    t1 = threading.Thread(target=imgur_upload, args=(args,links,fronts,f'f-{i}.jpg'))
-    t1.start()
-    t2 = threading.Thread(target=imgur_upload, args=(args,links,backs,f'b-{i}.jpg'))
-    t2.start()
-    t1.join()
-    t2.join()
+    for i in range(page_count):
+        fronts_image = Image.new("RGB", (4096, 4096))
+        backs_image = Image.new("RGB", (4096, 4096))
 
-def write_deck(deckJSON, args, links, num):
-    name = args.faction + str(num)
-    deckJSON = deckJSON.replace("DeckName", name)
-    deckJSON = deckJSON.replace("FrontImageURL", links[f'f-{i}.jpg'])
-    deckJSON = deckJSON.replace("BackImageUrl", links[f'b-{i}.jpg'])
-    deckJSON = deckJSON.replace("ReplaceGUID", f'{name}C')
-    deckJSON = deckJSON.replace("ReplaceGUID2", f'{name}D')
-    with open(args.savedObjectsFolder+name+".json", 'w') as deck:
-        deck.write(deckJSON)
- 
-if __name__ == '__main__':
-    
-    parser = argparse.ArgumentParser(description='Convert Privateer Press card pdfs to Tabletop Simulator saved deck objects.')
-    parser.add_argument('faction', type=str, help='name of the faction you are converting to decks.')
-    parser.add_argument('imgurClientID', type=str, help='clientID in imgur.')
-    parser.add_argument('imgurAPIKey', type=str, help='API key in imgur.')
-    parser.add_argument('savedObjectsFolder', type=str, help='Your Saved Objects folder, include two trailing slashes cuz I am lazy.')
+        for j in range(70):
+            if len(fronts) <= i * 70 + j:
+                continue
+            front = fronts[i * 70 + j].resize((width, height), Image.BICUBIC)
+            back = backs[i * 70 + j].resize((width, height), Image.BICUBIC).rotate(180)
+            fronts_image.paste(front, (j % 10 * width, (j // 10) * height))
+            backs_image.paste(back, (j % 10 * width, (j // 10) * height))
+        t_1 = threading.Thread(
+            target=drive_upload, args=(fronts_image, f"f-{i}.jpg", links)
+        )
+        t_1.start()
+        t_2 = threading.Thread(
+            target=drive_upload, args=(backs_image, f"b-{i}.jpg", links)
+        )
+        t_2.start()
+        t_1.join()
+        t_2.join()
+
+
+def write_deck(json, args, drive_links, num):
+    """Craft the JSON for your final TTS deck Saved Object"""
+    if args.name is None:
+        args.name = "Warmachine"
+    name = args.name + str(num)
+    json = json.replace("DeckName", name)
+    json = json.replace("FrontImageURL", drive_links[f"f-{num}.jpg"])
+    json = json.replace("BackImageUrl", drive_links[f"b-{num}.jpg"])
+    json = json.replace("ReplaceGUID", f"{name}C")
+    json = json.replace("ReplaceGUID2", f"{name}D")
+    if args.savedObjectsFolder is None:
+        args.savedObjectsFolder = ""
+    with open(args.savedObjectsFolder + name + ".json", "w") as deck:
+        deck.write(json)
+
+
+def convert():
+    """This converts a cardbundle.pdf (downloaded from Privateer Press) into
+       Tabletop Simulator deck Saved Objects."""
+    parser = argparse.ArgumentParser(
+        description="Convert Privateer Press card pdfs to Tabletop Simulator saved deck objects."
+    )
+    parser.add_argument(
+        "-name",
+        type=str,
+        help="your deck name - possibly the faction you are converting",
+    )
+    parser.add_argument(
+        "-savedObjectsFolder",
+        type=str,
+        help="your Saved Objects folder, include two trailing slashes cuz I am lazy.",
+    )
     args = parser.parse_args()
 
     # Strip out the card images from the Privateer Press pdfs.
-    cardFronts = []
-    cardBacks = []
-    infile = 'cardbundle.pdf'
+    card_fronts = []
+    card_backs = []
+    infile = "cardbundle.pdf"
     pages = convert_from_path(infile)
     for page in pages:
-        parse_images(cardFronts, cardBacks, page)
-    
+        parse_images(card_fronts, card_backs, page)
+
     # But we don't want the blank white cards.
-    filteredFronts = []
-    filteredBacks = []
-    for i in range(len(cardFronts)):
-        # I'd kinda rather do a .filter on these but I'm concerned that there'd be some random stray pixel that would put them outta sync.
-        if PIL.ImageOps.invert(cardFronts[i]).getbbox():
-            filteredFronts.append(cardFronts[i])
-            filteredBacks.append(cardBacks[i])
-    
+    # I'd rather do a .filter, but I'm concerned a stray pixel would put them outta sync.
+    filtered_fronts = []
+    filtered_backs = []
+    for i, card in enumerate(card_fronts):
+        if PIL.ImageOps.invert(card).getbbox():
+            filtered_fronts.append(card)
+            filtered_backs.append(card_backs[i])
+
     # Collate the cards into the image format Tabletop Simulator requires.
     links = {}
-    deckCount = len(cardFronts)//70+1
-    for i in range(deckCount):
-        package_page(args, links, filteredFronts, filteredBacks)
-    
-    # Wait for all of the imgur uploads to finish - nvm, this causes a MemoryError I'm too lazy to track down :p.
-    # for thread in threads:
-    #     thread.join()
+    deck_count = len(card_fronts) // 70 + 1
+    package_pages(filtered_fronts, filtered_backs, deck_count, links)
 
     # And let's shove em all in your Saved Objects folder :)
-    deckJSON = ''
-    with open('decktemplate.json', 'r') as deckTemplate:
-        deckJSON = deckTemplate.read()    
-    for i in range(deckCount):
-        write_deck(deckJSON, args, links, i)
+    deck_json = ""
+    with open("decktemplate.json", "r") as deck_template:
+        deck_json = deck_template.read()
+    for i in range(deck_count):
+        write_deck(deck_json, args, links, i)
+
+
+if __name__ == "__main__":
+    convert()
